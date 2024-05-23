4110f888b2a381ab50fda8e9b83ecb22a773d45d
David Zamora <d.zamorac@uniandes.edu.co>
2024-05-23 10:31:40 -0500

    servicio de proyecto

--- src/proyecto/proyecto.service.ts
+++ src/proyecto/proyecto.service.ts
@@ -1,4 +1,28 @@
+
+/* eslint-disable prettier/prettier */
+/* archivo: src/proyecto/proyecto.service.ts */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { ProyectoEntity } from './proyecto.entity/proyecto.entity';
+
 
 @Injectable()
-export class ProyectoService {}
+export class ProyectoService {
+   constructor(
+       @InjectRepository(ProyectoEntity)
+       private readonly proyectoRepository: Repository<ProyectoEntity>
+   ){}
+
+  
+   async create(proyecto: ProyectoEntity): Promise<ProyectoEntity> {
+
+    if(proyecto.fechaFin<proyecto.fechaInicio){
+        throw new BusinessLogicException("La propuesta tiene una fecha de finalizacion anterior a la de inicio", BusinessError.NOT_FOUND);
+    }
+       return await this.proyectoRepository.save(proyecto);
+   }
+
+
+}
