3ac5e4a3bdf31e7edf2ddabeec9fc153ebbc4915
David Zamora <d.zamorac@uniandes.edu.co>
2024-05-23 10:27:30 -0500

    servicios de profesor y propuesta

--- src/profesor/profesor.service.ts
+++ src/profesor/profesor.service.ts
@@ -1,4 +1,65 @@
+
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { ProfesorEntity } from './profesor.entity/profesor.entity';
+
 
 @Injectable()
-export class ProfesorService {}
+export class ProfesorService {
+   constructor(
+       @InjectRepository(ProfesorEntity)
+       private readonly profesorRepository: Repository<ProfesorEntity>
+   ){}
+
+   async findOne(id: string): Promise<ProfesorEntity> {
+       const profesor: ProfesorEntity = await this.profesorRepository.findOne({where: {id}, relations: ["propuesta"] } );
+       if (!profesor)
+         throw new BusinessLogicException("The profesor with the given id was not found", BusinessError.NOT_FOUND);
+  
+       return profesor;
+   }
+  
+   async create(profesor: ProfesorEntity): Promise<ProfesorEntity> {
+        if(!(profesor.grupoInvestigacion =="TICSW" || profesor.grupoInvestigacion == "IMAGINE" || profesor.grupoInvestigacion == "COMIT" )){
+            throw new BusinessLogicException("The researching group is not valid", BusinessError.PRECONDITION_FAILED);
+        }else{
+            return await this.profesorRepository.save(profesor);
+        }
+       
+   }
+
+
+   async delete(id: string) {
+       const profesor: ProfesorEntity = await this.profesorRepository.findOne({where:{id}});
+       let number = 0;
+        profesor.propuestas.forEach(propuesta=> {
+            if (propuesta.proyecto){
+                number+=1;
+            }
+        });
+
+
+       if (number ==0)
+         throw new BusinessLogicException("The profesor cannot be deleted since a project exists", BusinessError.PRECONDITION_FAILED);
+    
+       await this.profesorRepository.remove(profesor);
+   }
+
+   async deleteByCedula(cedula: number) {
+    const profesor: ProfesorEntity = await this.profesorRepository.findOne({where:{cedula}});
+    let number = 0;
+     profesor.propuestas.forEach(propuesta=> {
+         if (propuesta.proyecto){
+             number+=1;
+         }
+     });
+
+
+    if (number ==0)
+      throw new BusinessLogicException("The profesor cannot be deleted since a project exists", BusinessError.PRECONDITION_FAILED);
+ 
+    await this.profesorRepository.remove(profesor);
+}
+}
--- src/propuesta/propuesta.service.ts
+++ src/propuesta/propuesta.service.ts
@@ -1,4 +1,42 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { PropuestaEntity } from './propuesta.entity/propuesta.entity';
+
 
 @Injectable()
-export class PropuestaService {}
+export class PropuestaService {
+   constructor(
+       @InjectRepository(PropuestaEntity)
+       private readonly propuestaRepository: Repository<PropuestaEntity>
+   ){}
+
+   async findAll(): Promise<PropuestaEntity[]> {
+       return await this.propuestaRepository.find({ relations: ["profesor", "proyecto"] });
+   }
+
+   async findOne(id: string): Promise<PropuestaEntity> {
+       const propuesta: PropuestaEntity = await this.propuestaRepository.findOne({where: {id}, relations: ["profesor", "proyecto"] } );
+       if (!propuesta)
+         throw new BusinessLogicException("The propuesta with the given id was not found", BusinessError.NOT_FOUND);
+  
+       return propuesta;
+   }
+  
+   async create(propuesta: PropuestaEntity): Promise<PropuestaEntity> {
+    if(propuesta.titulo=="" || !propuesta.titulo){
+        throw new BusinessLogicException("La propuesta no tiene titulo", BusinessError.PRECONDITION_FAILED)
+    }
+       return await this.propuestaRepository.save(propuesta);
+   }
+
+
+   async delete(id: string) {
+       const propuesta: PropuestaEntity = await this.propuestaRepository.findOne({where:{id}});
+       if (!propuesta.proyecto)
+         throw new BusinessLogicException("La propuesta no se puede eliminar porque tiene un  proyecto", BusinessError.NOT_FOUND);
+       await this.propuestaRepository.remove(propuesta);
+   }
+}
